0325821c488caf8b0828cec05298ce6488926eb5
stopdori <yungoo123@gmail.com>
2026-02-21 19:29:03 +0900

    fix robots route

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,11 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
     sitemap: "https://loltip.com/sitemap.xml",
   };
 }
