3e4943c8506d4a420841c236f7010df5d8705375
stopdori <yungoo123@gmail.com>
2026-02-21 10:23:57 +0900

    add root route

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,5 @@
-import { redirect } from "next/navigation"; export default function Page() { redirect("/champ"); }
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/champ");
+}
