8301141e891b171285a8992d9a1ff0d225b20545
stopdori <yungoo123@gmail.com>
2026-06-19 11:57:43 +0900

    Update Mordekaiser vs Shen matchup

--- app/data/matchups/mordekaiser/mordekaiser_shen.ts
+++ app/data/matchups/mordekaiser/mordekaiser_shen.ts
@@ -9,7 +9,7 @@ export const mordekaiser_shen: MatchupSummary = {
   },
   highlightsByChamp: {
     mordekaiser: {
-      ko: ["R의 [[BANISH]]으로 쉔 R의 [[SKILL_CHANNEL]]을 끊을 수 없음. [[NOT_EXIST]] \n 단, 쉔 R은 정상적으로 작동하지만 [[BLINK]] 경우에 따라 다름. \n  1. 쉔 R의 대상자가 위상이 다르지만 죽음의 세계 [[AOE]] 내에 위치할 때 = 대상의 위치에 [[BLINK]]. \n 2. 쉔 R의 대상자가 죽음의 세계 [[AOE]] 밖에 있을 때 = 죽음의 세계 안에서 대상자에게 가장 가까운 가장자리로 [[BLINK]]."],
+      ko: ["R의 [[BANISH]]으로 쉔 R의 [[SKILL_CHANNEL]]을 끊을 수 없음. [[NOT_EXIST]] \n 단, 쉔 R은 정상적으로 작동하지만 [[BLINK]] 경우에 따라 다름. \n  1. 쉔 R의 대상자가 위상이 다르지만 죽음의 세계 [[AOE]] 내에 위치할 때 = 대상의 위치에 [[BLINK]]. \n 2. 쉔 R의 대상자가 죽음의 세계 [[AOE]] 밖에 있을 때 = 죽음의 세계 안에서 대상자에게 가장 가까운 가장자리로 [[BLINK]]. [[CLIP:https://www.youtube.com/shorts/D4SucWomGyE]]"],
       en: [""],
     },
     shen: {
